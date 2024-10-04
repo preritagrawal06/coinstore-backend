c4a441925019e38473f81753e9e9498a47570bf7
preritagrawal06 <agrawalprerit780@gmail.com>
2024-10-04 16:19:49 +0530

    API added

--- Controllers/TopupController/index.js
+++ Controllers/TopupController/index.js
@@ -1,35 +1,26 @@
 const axios = require('axios')
-const { wrapper } = require('axios-cookiejar-support');
-const tough = require('tough-cookie');
-
-// Create a cookie jar to store cookies
-const cookieJar = new tough.CookieJar();
-
-// Wrap Axios instance to support cookie jar
-const client = wrapper(axios.create({ jar: cookieJar }));
 
 const api = axios.create({
     baseURL: "https://dev.api.elitedias.com",
-    headers:{
-        Origin: "https://google.com",
-        "Content-Type": "application/json",
-        Accept: "*/*",
-        "Cache-Control": "no-cache",
-        Host: "dev.api.elitedias.com",
-        "Accept-Encoding": "gzip, deflate, br",
-        Connection: "keep-alive",
-        "Content-Length": "365" 
-    },
 })
 
 const getAllGames = async (req, res, next)=>{
-
     try {
         const {data} = await api.post('/elitedias_games_available', {
             "api_key": process.env.API_KEY,
+        },{
+            headers:{
+                Origin: "https://google.com"
+            }
         })
-        
-        
+        if(data.code === '200'){
+            return res.json(data)
+        }else{
+            return res.json({
+                success: false,
+                message: "Some unknown error occured"
+            })
+        }
     } catch (error) {
         return res.json({
             success: false,
