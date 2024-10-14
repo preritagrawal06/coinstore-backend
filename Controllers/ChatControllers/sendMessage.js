c0b294ad0b43373ce55853a4b322ce6e5e16091d
preritagrawal06 <agrawalprerit780@gmail.com>
2024-10-14 20:10:41 +0530

    removed chat model

--- Controllers/ChatControllers/sendMessage.js
+++
@@ -1,2 +0,0 @@
-const {Chat} = require('../../Models')
-
--- Models/index.js
+++ Models/index.js
@@ -2,7 +2,5 @@ const Buyer = require('./buyerModel')
 const Seller = require('./sellerModel')
 const Account = require('./accountModel')
 const Admin = require('./adminModel')
-const Chat = require('./chatModel')
-const Room = require('./roomModel')
 
-module.exports = {Buyer, Seller, Account, Admin, Chat, Room}
+module.exports = {Buyer, Seller, Account, Admin}
