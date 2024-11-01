5be81f66863515f1d2c19948c203ef1279a1fae8
preritagrawal06 <agrawalprerit780@gmail.com>
2024-11-02 01:56:56 +0530

    update topup route added

--- Routes/topup.routes.js
+++ Routes/topup.routes.js
@@ -7,5 +7,5 @@ router.post('/get-topup-list', getGameTopupList)
 router.post('/check-id', checkGameID)
 router.post('/get-required-fields', getRequiredGameFields)
 router.post('/create-topup-order', resellerTopup)
-router.post('/update-topup', updateTopup) // TODO: add admin access middleware
+router.get('/update-topup', updateTopup) // TODO: add admin access middleware
 module.exports = router
