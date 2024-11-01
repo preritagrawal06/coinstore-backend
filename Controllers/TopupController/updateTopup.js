33a3015614c340b800ab9b39b9013e28ddd01960
preritagrawal06 <agrawalprerit780@gmail.com>
2024-11-02 01:59:22 +0530

    update topup route added

--- Controllers/TopupController/updateTopup.js
+++ Controllers/TopupController/updateTopup.js
@@ -138,7 +138,7 @@ const updateTopup = async (req, res) => {
     } catch (error) {
         return res.json({
             success: false,
-            message: "something went wrong"
+            message: error
         })
     }
 };
