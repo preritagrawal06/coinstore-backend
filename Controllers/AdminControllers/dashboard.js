c5570fe25d902bb31f4628ef52895c33124639e2
preritagrawal06 <agrawalprerit780@gmail.com>
2024-12-29 20:46:04 +0530

    elitedias reseller balance api added

--- Controllers/AdminControllers/dashboard.js
+++ Controllers/AdminControllers/dashboard.js
@@ -10,6 +10,7 @@ const dashboardDetails = async(req, res)=>{
     const purchaseGraph = await Transaction.aggregate(purchaseEachDay)
     const userGraph = await Buyer.aggregate(newUserEachDay)
     let balance
+    let usd
     try {
         const {data} = await axios.post('https://dev.api.elitedias.com/elitedias_api_balance', {
             "api_key": process.env.API_KEY
@@ -18,7 +19,9 @@ const dashboardDetails = async(req, res)=>{
                 "Origin": "www.google.com"
             }
         })
+        const { data: usdData } = await axios.get("https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/usd.min.json")
         balance = data
+        usd = usdData.usd.inr
     } catch (error) {
         balance = null
     }
@@ -27,7 +30,7 @@ const dashboardDetails = async(req, res)=>{
         userCount,
         depositTotal: deposit[0],
         purchaseTotal: purchase[0],
-        elitediasBalance: balance,
+        elitediasBalance: (balance.reseller_balance*usd).toFixed(2),
         purchaseGraph,
         userGraph
     })
